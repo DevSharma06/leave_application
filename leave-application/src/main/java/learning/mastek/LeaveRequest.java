faa5e2f6d82d28f367411b4cb5f540cc63d03e27
dev <dev@dev-sharma.com>
2019-03-21 20:03:20 +0530

    added a file LeaveRequest.java

---
+++ leave-application/src/main/java/learning/mastek/LeaveRequest.java
@@ -0,0 +1,20 @@
+package learning.mastek;
+
+public class LeaveRequest {
+
+	int requestId;
+	String reason;
+	public int getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(int requestId) {
+		this.requestId = requestId;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	
+}
