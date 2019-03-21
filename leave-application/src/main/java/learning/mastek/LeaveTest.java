c8a070173fcc0c8669e9afdb188142223048e2b9
dev <dev@dev-sharma.com>
2019-03-21 19:12:04 +0530

    Updated LeaveTest.java

--- leave-application/src/main/java/learning/mastek/LeaveTest.java
+++ leave-application/src/main/java/learning/mastek/LeaveTest.java
@@ -13,23 +13,29 @@ public class LeaveTest {
 		
 		System.out.println("Login:");
 		empno = sc.nextInt();
-		System.out.println("1. View Applied Leaves\r\n" + 
-				"			  2. Apply For Leave\r\n" + 
-				"			  3. Cancel Leave\r\n" + 
-				"			  4. View Leave Summary");
-		opt = sc.nextInt();
-		switch(opt)
+		while(opt != 5)
 		{
-		case 1: viewAppliedLeaves();
-			break;
-		case 2: applyLeave();
-			break;
-		case 3: cancelLeave();
-			break;
-		case 4: viewLeaveSummary();
-			break;
-		default:
-				System.out.println("Enter valid option");
+			System.out.println("1. View Applied Leaves\r\n" + 
+					"2. Apply For Leave\r\n" + 
+					"3. Cancel Leave\r\n" + 
+					"4. View Leave Summary"+
+					"5. Exit\n");
+			opt = sc.nextInt();
+			switch(opt)
+			{
+			case 1: viewAppliedLeaves();
+				break;
+			case 2: applyLeave();
+				break;
+			case 3: cancelLeave();
+				break;
+			case 4: viewLeaveSummary();
+				break;
+			case 5: System.out.println("Thank you");
+				break;
+			default:
+					System.out.println("Enter valid option");
+			}
 		}
 		
 	}
